845eacf8bb9300009f5e31795148f29b1d8f43ce
ghost <ghost@192.168.1.8>
2024-07-14 16:24:57 -0400

    Configuracion de los componentes del app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import LeftBar from "./components/LeftBar";
+import Main from "./components/Main";
+
+export default function App() {
+    return (
+        <>
+            <LeftBar />
+            <Main />
+        </>
+    );
+}
---
+++ src/components/LeftBar.tsx
@@ -0,0 +1,7 @@
+export default function LeftBar() {
+    return (
+        <div className="left-bar">
+            <h1>LeftBar</h1>
+        </div>
+    );
+}
